274f415eeba3ff0123dd0e1979ff3f9d5413b921
leeburgess67 <leeburgessmail@gmail.com>
2018-08-06 18:09:55 +0100

    Working with button now

--- src/views/input_view.js
+++ src/views/input_view.js
@@ -4,20 +4,23 @@ const InputView = function () {
 
 };
 
-//Make bindEvents function
+//Make bindEvents function which will be called in the app.js
 InputView.prototype.bindEvents = function () {
 
 //Declare the input as the text inputted - id  'text' on input form
 const input = document.querySelector('#text')
+const button = document.querySelector('#button')
 
 //add event listener, when there is an input do something (event)...
-input.addEventListener('input', (evt) => {
-
+// input.addEventListener('input', (evt) => {
+button.addEventListener('click', (evt) => {
+evt.preventDefault();
   // declare the inputted text to be the event targetted above
   const inputtedText = evt.target.value
 
   // publish the inputted text with this name which will be picked up.
   PubSub.publish('InputView:text-inputted', inputtedText)
+  
 });
 };
 
