94182287283c79c35cdf2e9d61b1e82ffc50eb6a
leeburgess67 <leeburgessmail@gmail.com>
2018-08-06 16:00:01 +0100

    result view completed

---
+++ src/views/result-view.js
@@ -0,0 +1,20 @@
+const PubSub = require('../helpers/pub_sub.js')
+const ResultView = function () {
+};
+
+//CREATE A BINDEVENTS FUNCTION TO SUBSCRIBE TO THE WORDCOUNTER AND DISPLAY THE DATA
+
+ResultView.prototype.bindEvents = function () {
+  PubSub.subscribe('WordCounter:result', (evt) => {
+    const result = evt.detail;
+    this.updateView(result);
+  });
+};
+
+ResultView.prototype.updateView = function (result) {
+  const resultElement = document.querySelector('#result'){
+    resultElement.textContent = `There are ${result} words`
+  }
+
+
+};
